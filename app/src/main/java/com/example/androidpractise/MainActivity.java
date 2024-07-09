61d7dd319b6bccf81544da5c32c2624dd1bb305a
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-09 22:13:13 +0700

    Android Practise

--- app/src/main/java/com/example/androidpractise/MainActivity.java
+++ app/src/main/java/com/example/androidpractise/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.androidpractise;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,22 +13,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    public void onClick(View v) {
+//        switch(v.getId()){
+//            case R.id.btnWelcome :
+//                Toast.makeText(this,"Hello Button Clicked",Toast.LENGTH_LONG).show();
+//                break;
+//            default:
+//                break;
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.btn_welcome);
-        btn.setOnClickListener(this);
+        Button btn = findViewById(R.id.btnWelcome);
+       // btn.setOnClickListener(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView txt = findViewById(R.id.txt);
+                EditText edt = findViewById(R.id.editTxt);
+                txt.setText("Hello "+edt.getText().toString()+"");
+                //edt.clear();
+
+            }
+        });
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this,"Long Clicked",Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        TextView txt = findViewById(R.id.txtWelcome);
-        txt.setText("Hello Android");
-    }
+
+
+
+
+//    @Override
+//    public void onClick(View v) {
+//        TextView txt = findViewById(R.id.txtWelcome);
+//        txt.setText("Hello Android");
+//    }
 
 //    public void OnHelloBtnClicked(View view){
 //        TextView txt = findViewById(R.id.txtWelcome);
