c29883ece3aa5991b54e31eb37c367b165c38da2
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-09 22:13:13 +0700

    Android Practise

--- app/src/main/java/com/example/androidpractise/MainActivity.java
+++ app/src/main/java/com/example/androidpractise/MainActivity.java
@@ -23,40 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.btnWelcome);
-       // btn.setOnClickListener(this);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView txt = findViewById(R.id.txt);
-                EditText edt = findViewById(R.id.editTxt);
-                txt.setText("Hello "+edt.getText().toString()+"");
-                //edt.clear();
 
-            }
-        });
-        btn.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Toast.makeText(MainActivity.this,"Long Clicked",Toast.LENGTH_LONG).show();
-                return true;
-            }
-        });
     }
-
-
-
-
-
-//    @Override
-//    public void onClick(View v) {
-//        TextView txt = findViewById(R.id.txtWelcome);
-//        txt.setText("Hello Android");
-//    }
-
-//    public void OnHelloBtnClicked(View view){
-//        TextView txt = findViewById(R.id.txtWelcome);
-//        txt.setText("Hello Android");
-//    }
-
 }
