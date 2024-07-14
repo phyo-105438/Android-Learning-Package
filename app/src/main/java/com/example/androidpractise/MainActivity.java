89749220beba7bc25d6d33ec8d9d7791481ba65b
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-14 17:52:01 +0700

    Android Practise

--- app/src/main/java/com/example/androidpractise/MainActivity.java
+++ app/src/main/java/com/example/androidpractise/MainActivity.java
@@ -10,27 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    Button btn_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btn);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(MainActivity.this,"Hello Android Developer",Toast.LENGTH_LONG).show();
-//            }
-//        });
-
-        btn.setOnClickListener(new Walifer());
     }
-    public class Walifer implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(MainActivity.this,"Hello Android Developer",Toast.LENGTH_LONG).show();
-        }
-    }
-
 }
